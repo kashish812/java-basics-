77b3c95565c246ae0eb5f6d2e92b15a0ce5f4f7e
Kashish Gahlot <kashishgahlot25@gmail.com>
2026-07-11 23:16:02 +0530

    Implement Bubble Sort in Descending Order

---
+++ Sorting/BubbleSortDescending.java
@@ -0,0 +1,28 @@
+public class BubbleSortDescending {
+
+    public static void main(String[] args) {
+
+        int arr[] = {2, 4, 5, 1, 0};
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (arr[j] < arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                }
+            }
+        }
+
+        System.out.print("Sorted Array: ");
+
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+    }
+}
