095da81f758f4ab426077ca770c5881850edfe95
Kashish Gahlot <kashishgahlot25@gmail.com>
2026-07-12 12:54:22 +0530

    Create BubbleSortOptimized.java

---
+++ Sorting/BubbleSortOptimized.java
@@ -0,0 +1,33 @@
+public class OptimizedBubbleSort {
+
+    public static void main(String[] args) {
+
+        int arr[] = {2, 4, 5, 1, 0};
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            boolean swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+    }
+}
